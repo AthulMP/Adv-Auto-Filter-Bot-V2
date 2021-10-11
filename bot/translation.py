01b4d3e58119240551085133b2090c2c4eca6aac
AthulMP <92137554+AthulMP@users.noreply.github.com>
2021-10-11 22:05:56 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,10 +7,10 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+നിങ്ങൾക്ക് എന്നെ മറ്റൊരു ഗ്രൂപ്പിലേക്ക് ആഡ് ചെയ്യാൻ പറ്റില്ല😢😪
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@MoviePlex_TG
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@MoviePlex_TG
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/UnKnOwN_HaCKeR_2_0">UNKNOWN HACKER</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/AthulMP/Adv-Auto-Filter-Bot-V2">Click Me</a></i>
 """
